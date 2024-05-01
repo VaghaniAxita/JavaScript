b64685a90cea01ee3fb3a373dd7e61af0d45e29b
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-05-01 14:58:07 +0530

    1/5/24 Lecture 13

--- Lecture 11/ArraytoLowerCase.js
+++ Lecture 11/ArraytoLowerCase.js
@@ -1,7 +1,13 @@
-let array=["RED","AND","WHITE"]
+let array = ["RED", "AND", "WHITE"]
 const print = (array) => {
     for (let i = 0; i < array.length; i++) {
-        console.log(array[i]);
+        if (i % 2 == 0) {
+            console.log(array[i]);
+        }
+        else
+        {
+            console.log(array[i].toLowerCase());
+        }
     }
 }
 print(array)
