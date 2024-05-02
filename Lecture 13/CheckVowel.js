da01f2c1d545b93eff741a6ae6b731bfa5af1436
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-05-02 15:04:04 +0530

    1/5/24 Lecture 13

---
+++ Lecture 13/CheckVowel.js
@@ -0,0 +1,15 @@
+let str = "red and white"
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] == "a" || str[i] == "e" || str[i] == "i" || str[i] == "o" || str[i] == "u") {
+        count++;
+    }
+}
+
+if (count > 0) {
+    console.log(true);
+}
+else {
+    console.log(false);
+} 
