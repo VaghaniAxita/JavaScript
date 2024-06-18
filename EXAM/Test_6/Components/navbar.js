8eba31e9dae89edfe8ab2c89d47cd7aa952e0eb0
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-06-18 07:44:39 +0530

    17/6/24 Test_6

--- EXAM/Test_6/Components/navbar.js
+++ EXAM/Test_6/Components/navbar.js
@@ -1,9 +1,11 @@
 const navbar = () => {
     return `<nav>
       <a href="/EXAM/Test_6/index.html">HOME</a>
-      <a href="/EXAM/Test_6/pages/Bookmark.html">BOOKMARK</a>
-      <a href="/EXAM/Test_6/pages/Dashboard.html">DASHBOARD</a>
+       <a href="/EXAM/Test_6/pages/Dashboard.html">DASHBOARD</a>
       <a href="/EXAM/Test_6/pages/MyBooks.html">MYBOOKS</a>
+      <a href="/EXAM/Test_6/pages/Bookmark.html">BOOKMARK</a>
+     
+      
 
         </nav>`;
 };
--- EXAM/Test_6/js/Bookmark.js
+++ EXAM/Test_6/js/Bookmark.js
@@ -1,26 +1,54 @@
+// import navbar from "../Components/navbar.js";
+// document.getElementById("navbar").innerHTML = navbar();
+// let books = JSON.parse(localStorage.getItem('bookmark-list')) || [];
+// const displayBooks = (books) => {
+//     const tableBody = document.querySelector("#tbody");
+//     tableBody.innerHTML = ''; 
+
+//     books.forEach((book) => {
+//         const row = document.createElement('tr');
+
+//         row.innerHTML = `
+//             <td>${book.name}</td>
+//             <td>${book.author}</td>
+//             <td>${book.description}</td>
+//             <td>${book.date}</td>
+//             <td>${book.category}</td>
+//             <td>${book.price ? book.price : 'N/A'}</td>
+//         `;
+
+//         tableBody.appendChild(row);
+//     });
+// };
+
+// window.onload = () => {
+//     displayBooks(bookmarkList);
+// };
+
+
 import navbar from "../Components/navbar.js";
 document.getElementById("navbar").innerHTML = navbar();
 let books = JSON.parse(localStorage.getItem('bookmark-list')) || [];
 const displayBooks = (books) => {
-    const tableBody = document.querySelector("#tbody");
-    tableBody.innerHTML = ''; 
+    const tableBody = document.querySelector("#bookmarkTable tbody");
+    tableBody.innerHTML = '';
 
-    books.forEach((book) => {
-        const row = document.createElement('tr');
-        
-        row.innerHTML = `
+    const rows = books.map((book) => `
+        <tr>
             <td>${book.name}</td>
             <td>${book.author}</td>
             <td>${book.description}</td>
             <td>${book.date}</td>
             <td>${book.category}</td>
             <td>${book.price ? book.price : 'N/A'}</td>
-        `;
+        </tr>
+    `).join('');
 
-        tableBody.appendChild(row);
-    });
+
+    tableBody.innerHTML = rows;
 };
 
+
 window.onload = () => {
-    displayBooks(bookmarkList);
+    displayBooks(books);
 };
