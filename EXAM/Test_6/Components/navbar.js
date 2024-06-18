60b68c13b4bcb5d789b987c1fee292b235c8cfda
VaghaniAxita <vaghaniaxita23@gmail.com>
2024-06-18 07:25:33 +0530

    17/6/24 Test_6

--- EXAM/Test_6/Components/navbar.js
+++ EXAM/Test_6/Components/navbar.js
@@ -1,10 +1,12 @@
 const navbar = () => {
-    return `  <nav>
-            <a href="test_6/index.html">Home</a>
-            <a href="/test_6/pages/Dashboard.html">Dashboard</a>
-            <a href="/test_6/pages/MyBooks.html">My Books</a>
-            <a href="/test_6/pages/Bookmark.html">Bookmark</a>
+    return `<nav>
+      <a href="/EXAM/Test_6/index.html">HOME</a>
+      <a href="/EXAM/Test_6/pages/Bookmark.html">BOOKMARK</a>
+      <a href="/EXAM/Test_6/pages/Dashboard.html">DASHBOARD</a>
+      <a href="/EXAM/Test_6/pages/MyBooks.html">MYBOOKS</a>
+
         </nav>`;
 };
 
 export default navbar;
+
